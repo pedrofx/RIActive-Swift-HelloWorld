b3da7a2aa7b09c90bf7590d25541c6a40f92f971
yacaFx <sergiobritor@gmail.com>
2014-07-06 23:41:02 -0500

    Initial Commit

---
+++ Hola Mundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hola Mundo
+//
+//  Created by Sergio Brito on 7/6/14.
+//  Copyright (c) 2014 RIActive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
