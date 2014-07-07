849997468c725bc7d51a67d6950b2cca947082f4
yacaFx <sergiobritor@gmail.com>
2014-07-06 23:43:32 -0500

    Create Swift Project

--- Hola Mundo/AppDelegate.swift
+++ Hola Mundo/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Hola Mundo
 //
-//  Created by Sergio Brito on 7/6/14.
+//  Created by Sergio Brito aka @yacaFx on 7/6/14.
 //  Copyright (c) 2014 RIActive. All rights reserved.
 //
 
