9c8a3049fe93b4c370f306d69a476fff33479a04
yacaFx <sergiobritor@gmail.com>
2014-07-06 23:58:52 -0500

    Create Swift Project

--- Hola MundoTests/Hola_MundoTests.swift
+++ Hola MundoTests/Hola_MundoTests.swift
@@ -2,7 +2,7 @@
 //  Hola_MundoTests.swift
 //  Hola MundoTests
 //
-//  Created by Sergio Brito on 7/6/14.
+//  Created by Sergio Brito aka @yacaFx on 7/6/14.
 //  Copyright (c) 2014 RIActive. All rights reserved.
 //
 
